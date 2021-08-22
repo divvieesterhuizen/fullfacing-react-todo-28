97031ab896b7047dee39d5ceecad003bccd9b59b
Divvie Esterhuizen <divvieesterhuizen@Divvies-MacBook-Air-2017-Big-Sur.local>
2021-08-22 20:54:17 +0200

    delete method added

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,16 +1,40 @@
-const TodoItem = ({ todo }) => {
+import { FaTrash } from 'react-icons/fa';
+import { FaPencilAlt } from 'react-icons/fa';
+
+import styles from '../styles/Todo.module.css';
+
+const TodoItem = ({ todo, deleteTodo }) => {
   const { id, title, completed } = todo;
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'd' || e.key === 'D') {
+      deleteTodo(id);
+    }
+  };
+
   return (
-    <div className='flex'>
+    <div className='flex flex-justify'>
       <input
         type='checkbox'
         onChange={() => {
-          onCheck(id);
+          console.log('checked: ', title);
         }}
         checked={!!completed}
+        onKeyPress={handleKeyPress}
       />
-      <h4>{title}</h4>
+      <h4 className={styles.todoText}>{title}</h4>
+      <FaTrash
+        onClick={() => {
+          deleteTodo(id);
+        }}
+        className={`${styles.todoIcon} red`}
+      />
+      <FaPencilAlt
+        onClick={() => {
+          onEditClick();
+        }}
+        className={`${styles.todoIcon} green`}
+      />
     </div>
   );
 };
--- components/TodoList.js
+++ components/TodoList.js
@@ -10,17 +10,23 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
 
   // Functions
-  const addTodo = async (text) => {
+  const addTodo = (text) => {
     const newTodo = { id: nanoid(), title: text, completed: false };
     console.log(newTodo);
     setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
     <>
       <AddTodo addTodo={addTodo} />
       {todos.length > 0 ? (
-        todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+        todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+        ))
       ) : (
         <h4>No Todos to show. Maybe add one using the form above.</h4>
       )}
